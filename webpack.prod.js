96ff7a6430f0a24f27869b587f6929a2776d8906
ADDFa <fratamaadha18@gmail.com>
2025-03-07 16:14:25 +0700

    edit readme.md

---
+++ webpack.prod.js
@@ -0,0 +1,10 @@
+const path = require("path")
+
+module.exports = {
+    entry: "./app/Resources/js/main.js",
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "public/assets/js")
+    },
+    mode: "production"
+}
